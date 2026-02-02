173e4194431ab231673eab7700f7bd3070f6c570
albertumcat-boop <albert.umcat@gmail.com>
2026-02-02 14:22:12 -0400

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,14 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY_REAL", 
-  authDomain: "TU_DOMINIO_REAL",
-  projectId: "TU_ID_REAL",
-  storageBucket: "TU_BUCKET_REAL",
-  messagingSenderId: "TU_SENDER_ID_REAL",
-  appId: "TU_APP_ID_REAL"
+  apiKey: "AIzaSyBd7HDH1wDbkq8T1XTsKUV0gBQ_O_BF7jI",
+  authDomain: "avicolapet.firebaseapp.com",
+  projectId: "avicolapet",
+  storageBucket: "avicolapet.firebasestorage.app",
+  messagingSenderId: "909478828234",
+  appId: "1:909478828234:web:b04a444b41d239cc4a3c33",
+  measurementId: "G-S1SBZT5QHW"
 };
 
+// Inicializamos Firebase y la Base de Datos (Firestore)
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
