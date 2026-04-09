a6c8b627fae494e42c378d427725dd47879c4326
albertumcat-boop <albert.umcat@gmail.com>
2026-04-09 16:10:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 //   • Manejo de errores robusto para no romper la app
 // =========================================================
 
-import { db } from '../firebase-config.js';
+import { db } from './firebase-config.js';
 import {
   doc, getDoc, addDoc, collection, serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
